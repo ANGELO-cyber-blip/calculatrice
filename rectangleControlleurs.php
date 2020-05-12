ecadb74ca93ace3f7313f74e96dea7bdc0a42a05
ANGELO-cyber-blip <61950495+ANGELO-cyber-blip@users.noreply.github.com>
2020-05-12 13:02:20 +0000

    Create rectangleControlleurs.php

---
+++ rectangleControlleurs.php
@@ -0,0 +1,16 @@
+#rectangleControlleurs.php
+  <?php
+function demi_Perimetre($longueur,$largeur){
+    return $longueur+$largeur ;
+}
+function perimetre($longueur,$largeur){
+    return demi_perimetre($longueur,$largeurlargeur);
+
+}
+function surface($longueur, $largeur){
+    return $longueur*$largeur;
+}
+function diagonale($longueur, $largeur){
+    return sqrt(pow($longueur,2)+pow($largeur))
+}
+?>
